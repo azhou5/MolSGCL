7e1b9720d6dfb91ba95fc0e814d8401be2cec0d4
Andrew Zhou-anz226 <azhou@hms.harvard.edu>
2025-08-30 11:11:14 -0400

    removing identifying info

--- code/run_minimol_msgcl.sh
+++ code/run_minimol_msgcl.sh
@@ -1,20 +1,3 @@
-#!/bin/bash
-#SBATCH -c 1
-#SBATCH -t 1:10:00
-#SBATCH -p short
-#SBATCH --mem=50G
-#SBATCH -o run_logs/minimol_%j.out
-#SBATCH -e run_logs/minimol_%j.err
-
-# -------------------------------
-# SLURM job for Minimol grid search
-# -------------------------------
-
-module load conda/miniforge3/24.11.3-0
-module load gcc/14.2.0
-eval "$(conda shell.bash hook)"
-conda activate minimol-env
-
 
 WORKDIR=".."
 SCRIPT="$WORKDIR/code/minimol_triplet_runner.py"  
@@ -35,12 +18,12 @@ mkdir -p "$OUTPUT_BASE/$RUN_ID"
 python -u code/minimol_triplet_runner.py \
   --csv_path $INPUT_CSV \
   --output_dir $OUTPUT_BASE/$RUN_ID \
-  --cache_file "/n/data1/hms/dbmi/farhat/anz226/Lyme_AZ/cache/smiles_fp_cache.pt" \
+  --cache_file "<INSERT MINIMIOL FINGERPRINT CACHE FILE PATH HERE>" \
   --lrs "3e-4" \
   --epochs "100" \
   --batch_size 128 \
   --margins "0.25" \
-  --triplet_weights "200" \
+  --triplet_weights "400" \
   --max_frags_per_mol 30\
   --replicates 25 \
   --task "$TASK" \
